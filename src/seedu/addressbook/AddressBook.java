ce84265ab2b4baec61162704df483f2f1308f61e
Vesali <vesali@Vesalis-MacBook-Pro.local>
2017-01-23 19:09:20 +0800

    changed method name removePrefixSign to removePrefix

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -1051,12 +1051,12 @@ public class AddressBook {
 
 		// phone is last arg, target is from prefix to end of string
 		if (indexOfPhonePrefix > indexOfEmailPrefix) {
-			return removePrefixSign(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
+			return removePrefix(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
 					PERSON_DATA_PREFIX_PHONE);
 
 			// phone is middle arg, target is from own prefix to next prefix
 		} else {
-			return removePrefixSign(encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
+			return removePrefix(encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
 					PERSON_DATA_PREFIX_PHONE);
 		}
 	}
@@ -1074,12 +1074,12 @@ public class AddressBook {
 
 		// email is last arg, target is from prefix to end of string
 		if (indexOfEmailPrefix > indexOfPhonePrefix) {
-			return removePrefixSign(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
+			return removePrefix(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
 					PERSON_DATA_PREFIX_EMAIL);
 
 			// email is middle arg, target is from own prefix to next prefix
 		} else {
-			return removePrefixSign(encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
+			return removePrefix(encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
 					PERSON_DATA_PREFIX_EMAIL);
 		}
 	}
@@ -1208,7 +1208,7 @@ public class AddressBook {
 	 *            Parameter sign to be removed
 	 * @return string without the sign
 	 */
-	private static String removePrefixSign(String s, String sign) {
+	private static String removePrefix(String s, String sign) {
 		return s.replace(sign, "");
 	}
 
